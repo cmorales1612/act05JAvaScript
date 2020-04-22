2441860b0471cfb883af4259bc279385b03f2bbf
cmorales1612 <60615615+cmorales1612@users.noreply.github.com>
2020-04-22 15:18:28 +0200

    Add files via upload

---
+++ m08uf2act05/js/m08uf2act05.js
@@ -0,0 +1,32 @@
+document.write ("Activitat 05 <br>");
+var i;
+for (i = 9; i < 10; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 18; i < 19; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 27; i < 28; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 36; i < 37; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 45; i < 46; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 54; i < 55; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 63; i < 64; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 72; i < 73; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 81; i < 82; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
+for (i = 90; i < 91; i++) {
+  document.write ("La variable i val: " + i + "<br/>");
+}
